9cd1520b1717e3f9bceab687a45ea05772e8c420
Masruful Islam <140105435+masrufulislam@users.noreply.github.com>
2025-06-21 14:22:13 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,33 +1,3 @@
-
-// function sendMail(event) {
-//     event.preventDefault();
-
-//     let name = document.getElementById("name").value.trim();
-//     let email = document.getElementById("email").value.trim();
-//     let subject = document.getElementById("subject").value.trim();
-//     let message = document.getElementById("message").value.trim();
-
-//     if (!name || !email || !subject || !message) {
-//         alert("Please fill out all fields before sending the message.");
-//         return; 
-//     }
-
-//     let parms = {
-//         name: name,
-//         email: email,
-//         subject: subject,
-//         message: message,
-//     };
-
-//     emailjs.send("service_lo8y6ge", "template_3137dfe", parms).then(() => {
-//             alert("Thank you for contacting us. We will get back to you shortly.");
-//         })
-//         .catch((error) => {
-//             console.error("Email sending failed:", error);
-//             alert("Failed to send email. Please try again later.");
-//         });
-// }
-
 function sendMail(event) {
     event.preventDefault();
 
@@ -54,7 +24,7 @@ function sendMail(event) {
         priority: priority
     };
 
-    emailjs.send("service_lo8y6ge", "template_3137dfe", parms).then(() => {
+    emailjs.send("service_0v4esvx", "template_4x4cur5", parms).then(() => {
             alert("Thank you for contacting us. We will get back to you shortly.");
         })
         .catch((error) => {
